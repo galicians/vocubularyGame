8d536eff2a56a5effaa6ab333d60c3c1cc4e454a
galicians <galicians@gmail.com>
2014-11-09 19:38:40 +0000

    more e2e tests added

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -9,11 +9,21 @@ describe('busuuApp', function() {
     })
 
     it("should display a question", function() {
-
       var question = element(by.binding('question'))
-      
       expect(question).toBeDefined()
     })
+
+    it("should display 3 possible answers", function() {
+      var answers = element.all(by.repeater('answer in answers'))
+      expect(answers.count()).toEqual(3)
+    })
+
+    it("should display the submit button", function() {
+      var submit = element(by.id('submitAnswer'))
+      expect(submit).toBeDefined()
+    })
+
+
 
   })
 
